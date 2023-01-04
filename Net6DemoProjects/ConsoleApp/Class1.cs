2c4b140e354f44f2b646a152fed525c4f513a207
madhub <b.madhu@outlook.com>
2023-01-05 00:26:37 +0530

    record type demo

---
+++ Net6DemoProjects/ConsoleApp/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp;
+
+public record Person(string FirstName, string LastName);
+public record Person1
+{
+    public string FirstName { get; init; }
+    public string LastName { get; init; }
+}
+
+
+// using Record types for Poco https://github.com/dotnet/runtime/issues/46299
+public sealed record LogLevel
+{
+    public string LogCategory { get; init; }
+    public string logValue { get; init; }
+}
+public sealed record Logging
+{
+    public LogLevel LogLevel { get; init; }
+
+}
---
+++ Net6DemoProjects/ConsoleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.Extensions.Configuration;
+
+ConfigurationManager configurationManager = new ConfigurationManager();
+//configurationManager.SetBasePath(Directory.GetCurrentDirectory());
+configurationManager.AddJsonFile("appSettings.json");
+var loggingSection = configurationManager.GetSection("Logging");
+
+var log = loggingSection.Get<ConsoleApp.Logging>();
+Console.WriteLine(log);
+ConsoleApp.LogLevel l1 = new ConsoleApp.LogLevel() { LogCategory = "", logValue = "" };
+
+
