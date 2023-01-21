1b95c7d645b7d5a1b56c41a62ebd95aa926bfb1f
madhub <b.madhu@outlook.com>
2023-01-21 11:08:11 +0530

    added application configuration demo

---
+++ Net6DemoProjects/UsingConfigurationOption/ApplicationInfo.cs
@@ -0,0 +1,8 @@
+﻿namespace UsingConfigurationOption
+{
+    public record class ApplicationInfo
+    {
+        public string Name { get; init; } = string.Empty;
+        public string Id { get; init; } = string.Empty;
+    }
+}
---
+++ Net6DemoProjects/UsingConfigurationOption/Controllers/WeatherForecastController.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace UsingConfigurationOption.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly IOptions<ApplicationInfo> appInfo;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger,IOptions<ApplicationInfo> appInfo)
+        {
+            _logger = logger;
+            this.appInfo = appInfo;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
