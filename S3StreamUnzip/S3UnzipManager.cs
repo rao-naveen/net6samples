d5d8d9d9d2808b4dd29cef9ad82c4517de700c74
rao-naveen <rao.naveen@gmail.com>
2023-02-18 18:57:36 +0530

    Added download support
    Added support to download + unzip + upload

--- S3StreamUnzip/S3UnzipManager.cs
+++ S3StreamUnzip/S3UnzipManager.cs
@@ -35,6 +35,104 @@ namespace S3StreamUnzip
             this.amazonS3 = amazonS3;
             _logger = logger;
         }
+
+        public async Task<List<string>> DirectoryUnzipUsingCSharpziplib(string inputBucketName, string inputObjectKey,
+            string outputBucketName, string? outputprefix, string downloadDir)
+        {
+            var downloadFile = Path.Combine(downloadDir, inputObjectKey);
+            if (Path.GetExtension(downloadFile) != ".zip")
+            {
+                downloadFile += ".zip";
+            }
+            var extractedDir = Path.Combine(downloadDir, Path.GetFileNameWithoutExtension(inputObjectKey));
+            
+            await DownloadFileAsync(inputBucketName, inputObjectKey, downloadFile).ConfigureAwait(false);
+            ExtractZip(extractedDir, downloadFile);
+            await UploadDirectoryAsync(outputBucketName, outputprefix, extractedDir).ConfigureAwait(false);
+
+            var uploadedEntries = Cleanup(extractedDir, downloadFile);
+            return uploadedEntries;
+        }
+
+        /// <summary>
+        /// Cleans up the zip file and extracted dir
+        /// </summary>
+        /// <param name="extractedDir"></param>
+        /// <param name="downloadFile"></param>
+        /// <returns></returns>
+        private static List<string> Cleanup(string extractedDir, string downloadFile)
+        {
+            List<string> uploadedEntries = new List<string>();
+            var files = Directory.EnumerateFiles(extractedDir, "*.*", SearchOption.AllDirectories);
+            uploadedEntries.AddRange(files);
+            Directory.Delete(extractedDir, true);
+            File.Delete(downloadFile);
+            return uploadedEntries;
+        }
+
+        /// <summary>
+        /// Uploads the contents of a directory to output S3 bucket
+        /// </summary>
+        /// <param name="outputBucketName">Name of output bucket</param>
+        /// <param name="outputprefix">Prefix to use for output objects</param>
+        /// <param name="extractedDir">Directory to upload</param>
+        /// <returns></returns>
+        private async Task UploadDirectoryAsync(string outputBucketName, string? outputprefix, string extractedDir)
+        {
+            Stopwatch sw = Stopwatch.StartNew();
+            TransferUtilityUploadDirectoryRequest uploadRequest = new TransferUtilityUploadDirectoryRequest();
+            uploadRequest.BucketName = outputBucketName;
+            uploadRequest.KeyPrefix = outputprefix;
+            uploadRequest.Directory = extractedDir;
+            uploadRequest.SearchOption = SearchOption.AllDirectories;
+            uploadRequest.SearchPattern = "*.*";
+            uploadRequest.UploadFilesConcurrently = true;
+            TransferUtility tr = new TransferUtility(amazonS3);
+            await tr.UploadDirectoryAsync(uploadRequest).ConfigureAwait(false);
+            sw.Stop();
+            _logger.LogInformation($"Upload took {sw.ElapsedMilliseconds} ms");
+        }
+
+        /// <summary>
+        /// Extracts the downloaded zip into a directory
+        /// </summary>
+        /// <param name="extractedDir">Path where to extract</param>
+        /// <param name="downloadFile">Zip file</param>
+        private void ExtractZip(string extractedDir, string downloadFile)
+        {
+            Stopwatch sw = Stopwatch.StartNew();
+            FastZip fastZip = new FastZip();
+            if (!Directory.Exists(extractedDir))
+            {
+                Directory.CreateDirectory(extractedDir);
+            }
+
+            fastZip.ExtractZip(downloadFile, extractedDir, null);
+            _logger.LogInformation($"Extract took {sw.ElapsedMilliseconds} ms");
+            sw.Stop();
+        }
+
+        /// <summary>
+        /// Downloads the fle into local disk
+        /// </summary>
+        /// <param name="inputBucketName">Name of the bucket to download from</param>
+        /// <param name="inputObjectKey">Key of the input zip file</param>
+        /// <param name="downloadFile">Location on local disk to download</param>
+        /// <returns></returns>
+        private async Task DownloadFileAsync(string inputBucketName, string inputObjectKey, string downloadFile)
+        {
+            TransferUtilityDownloadRequest request = new TransferUtilityDownloadRequest();
+            request.BucketName = inputBucketName;
+            request.Key = inputObjectKey;
+            request.FilePath = downloadFile;
+
+            Stopwatch sw = Stopwatch.StartNew();
+            TransferUtility transferUtility = new TransferUtility(amazonS3);
+            await transferUtility.DownloadAsync(request).ConfigureAwait(false);
+            sw.Stop();
+            _logger.LogInformation($"Download took {sw.ElapsedMilliseconds} ms");
+        }
+
         /// <summary>
         /// Unzips the zipped object stored in s3 bucket.
         /// Uses CSharpZipLib library for unzip , preserving the directory structure
@@ -46,7 +144,7 @@ namespace S3StreamUnzip
         /// <param name="outputBucketName">output bucker where unzipped objects are stored</param>
         /// <param name="outputprefix">[optional]prefix to be used while constructing the output object key</param>
         /// <returns>Returns the list of objects key's in the outputbucker where unzipped files are stored.</returns>
-        public async Task<List<string>> UnzipUsingCSharpziplib(string inputBucketName, string inputObjectKey, string outputBucketName, string? outputprefix)
+        public async Task<List<string>> StreamUnzipUsingCSharpziplib(string inputBucketName, string inputObjectKey, string outputBucketName, string? outputprefix)
         {
 
             List<string> uploadedEntries = new List<string>();
@@ -70,7 +168,7 @@ namespace S3StreamUnzip
             //3. For each enty in Zip
             while ((theEntry = zipInputStream.GetNextEntry()) != null)
             {
-                _logger.LogDebug("Unzipping file {Name} of compression method {CompressionMethod}", theEntry.Name, theEntry.CompressionMethod);
+              //  _logger.LogDebug("Unzipping file {Name} of compression method {CompressionMethod}", theEntry.Name, theEntry.CompressionMethod);
 
                 if (theEntry.IsDirectory)
                 {
@@ -90,7 +188,7 @@ namespace S3StreamUnzip
                 // for debug print if it is file backed
                 if (!string.IsNullOrEmpty(fileBufferingReadStream.TempFileName))
                 {
-                    _logger.LogDebug("Temp file is created for {theEntry.Name} -> {fileBufferingReadStream.TempFileName} : {size}", theEntry.Name, fileBufferingReadStream.TempFileName, Bytes.GetReadableSize(theEntry.Size));
+                   // _logger.LogDebug("Temp file is created for {theEntry.Name} -> {fileBufferingReadStream.TempFileName} : {size}", theEntry.Name, fileBufferingReadStream.TempFileName, Bytes.GetReadableSize(theEntry.Size));
                 }
 
                 await using (stream)
@@ -99,7 +197,7 @@ namespace S3StreamUnzip
                     {
                         await UploadStreamToS3(outputBucketName, s3ObjectKey, stream);
                         uploadedEntries.Add(s3ObjectKey);
-                        _logger.LogInformation("Zip Entry upload successfully {s3ObjectKey}; Size {size}", s3ObjectKey, Bytes.GetReadableSize(theEntry.Size));
+                        //_logger.LogInformation("Zip Entry upload successfully {s3ObjectKey}; Size {size}", s3ObjectKey, Bytes.GetReadableSize(theEntry.Size));
                     }
                     catch (Exception e)
                     {
